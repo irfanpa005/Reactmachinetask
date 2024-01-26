a4ba071fac72f2a2e5cd9efb320e07f26a2f8a01
Ahammed Irfan Ali <69192055+irfanpa005@users.noreply.github.com>
2024-01-26 16:46:46 +0530

    greeting hour based

--- Front-End/src/components/TopNavbar/TopNavbar.jsx
+++ Front-End/src/components/TopNavbar/TopNavbar.jsx
@@ -3,10 +3,21 @@ import styles from './topnavbar.module.css';
 import profilePic from '../../assets/Rectangle 10.png'
 
 function TopNavbar({isSideBarOpen, setisSideBarOpen}) {
+  const [hour, setHour] = useState(new Date().getHours());
+
+  const getGreeting = () => {
+    if (hour < 12) {
+      return "Good morning";
+    } else if (hour >= 12 && hour < 17) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  };
 
   return (
     <div className={styles.topNavbarContainer}>
-      <p className={styles.goodMorning}>Good Morning ! 😊</p>
+      <p className={styles.goodMorning}>{getGreeting()} ! 😊</p>
       <div className={styles.userDetails}>
         <div>
             <p className={styles.userName}>John Doe</p>
