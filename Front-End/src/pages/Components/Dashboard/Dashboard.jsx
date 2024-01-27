a0077353ac8a1604b6038c84e891ce574b6bfadc
Ahammed Irfan Ali <69192055+irfanpa005@users.noreply.github.com>
2024-01-27 13:50:29 +0530

    responsive update

--- Front-End/src/pages/Components/Dashboard/Dashboard.jsx
+++ Front-End/src/pages/Components/Dashboard/Dashboard.jsx
@@ -12,16 +12,15 @@ function Dashboard() {
         <div className={`${styles.graphCol} col-md-8 col-12`}>
           <Graph />
         </div>
-        <div className={`${styles.pieCol} col-md-4 col-10`}>
+        <div className={`${styles.pieCol} col-md-4 col-12`}>
           <PieComponent />
         </div>
       </div>
-      <br></br>
       <div className="row">
         <div className={`${styles.tableCol} col-md-9 col-12`}>
           <Tabledata />
         </div>
-        <div className={`${styles.profCol} col-md-3 col-10`}>
+        <div className={`${styles.profCol} col-md-3 col-12`}>
           <Profile />
         </div>
       </div>
